0815ddbcc60cb274d3c0a906ca2757be354e0710
pozvo <pabloyarzun20@gmail.com>
2023-12-14 19:18:35 -0300

    Update settings.py

--- Inventario-master/Projecto_inventario/settings.py
+++ Inventario-master/Projecto_inventario/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vnt^3_+ih5l2@^jek-f@@g(j46gc+-#&h%tg4!-k!za=49qvd7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['18.220.61.231','http://18.220.61.231:8000/','172.31.36.245']
 
